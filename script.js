568dee44422dd753e91901abebe7af3048ed915f
Tanish Bhatta <tanishbhattaai@gmail.com>
2026-09-08 02:54:15 +0545

    init: Tic-Tac-Toe in javascript

---
+++ script.js
@@ -0,0 +1,19 @@
+const box11 = document.getElementById('o11');
+const box12 = document.getElementById('o12');
+const box13 = document.getElementById('o13');
+const box21 = document.getElementById('o21');
+const box22 = document.getElementById('o22');
+const box23 = document.getElementById('o23');
+const box31 = document.getElementById('o31');
+const box32 = document.getElementById('o32');
+const box33 = document.getElementById('o33');
+
+const array = [
+    [box11, box12, box13],
+    [box21, box22, box23],
+    [box31, box32, box33]
+];
+
+
+
+
