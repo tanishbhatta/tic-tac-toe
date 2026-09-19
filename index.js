784682316c1ff163308b375ce55d9cee9c86fa5b
Tanish Bhatta <tanishbhattaai@gmail.com>
2026-09-19 20:11:30 +0545

    created: state Array for init AI feature

--- index.js
+++ index.js
@@ -8,14 +8,16 @@ const box22 = document.getElementById('22');
 const box23 = document.getElementById('23');
 const box31 = document.getElementById('31');
 const box32 = document.getElementById('32');
-const box33 = document.getElementById('33'); //gotten all boxes with their positional values
+const box33 = document.getElementById('33'); //got all boxes by id with their positional values
 
+//board array of boxes
 const board = [
     [box11, box12, box13],
     [box21, box22, box23],
     [box31, box32, box33]
-]; //created a board array
+];
 
+//win condition array
 const winArray = [
     [[0,0], [0,1], [0,2]],
     [[1,0], [1,1], [1,2]],
@@ -27,12 +29,14 @@ const winArray = [
     [[0,2], [1,1], [2,0]]
 ];
 
+//svg markup for the 'O' player
 const O_MARKUP = `
         <svg viewBox="0 0 100 100" style="width:100%; height:100%; display:block;">
 
             <circle class="circle-box" cx="50" cy="50" r="28" stroke="var(--playerO)" stroke-width="4" fill="none" stroke-linecap="round" transform="rotate(-90 50 50)"/>
             </svg>`;
 
+//svg markup for the 'X' player          
 const X_MARKUP = `
         <svg viewBox="0 0 100 100" style="width:100%; height:100%; display:block">
 
@@ -40,18 +44,18 @@ const X_MARKUP = `
             <line class="cross-line" x1="75" y1="75" x2="25" y2="25" stroke="var(--playerX)" stroke-width="3.5"/>
         </svg>`
 
+//Functionality of drawing shapes (O, X) after we click on the box
 const updateBoardWithPlayerShape = (playerBool, position) => {
     if (playerBool) {
-        position.innerHTML = O_MARKUP;
-         //innerHtml for Circle in the game
+        position.innerHTML = O_MARKUP; //innerHtml for Circle in the game
         return false;
     }else{
-        position.innerHTML = X_MARKUP;
-        //innerHtml for Cross in the game
+        position.innerHTML = X_MARKUP; //innerHtml for Cross in the game
         return true;
     }
 }
 
+//Defining the condition for winning
 const winCond = () => {
     for (const i of winArray){
         if (board[i[0][0]][i[0][1]].innerHTML !== '' && board[i[0][0]][i[0][1]].innerHTML === board[i[1][0]][i[1][1]].innerHTML && board[i[0][0]][i[0][1]].innerHTML === board[i[2][0]][i[2][1]].innerHTML){
@@ -61,6 +65,7 @@ const winCond = () => {
     return [[], false];
 };
 
+//Taking coordinates of the winning combination of three boxes
 const getCoordOfBoxes = (tripleAsked) => {
     const [start, end] = [tripleAsked[0], tripleAsked[2]];
     const startCoords = [start[1] * 33.33 + 16.67, start[0] * 33.33 + 16.67];
@@ -68,15 +73,17 @@ const getCoordOfBoxes = (tripleAsked) => {
     return [startCoords, endCoords];
 };
 
+//Drawing line after the player's win
 const updateBoardAfterWin = (tripleAsked, playerBool) => {
     const[startCoords, endCoords] = getCoordOfBoxes(tripleAsked);
     const dashArrayLength = Math.sqrt(((endCoords[0] - startCoords[0]) ** 2) + ((endCoords[1] - startCoords[1]) ** 2)); //dash array length using Distance formula
+
     if (playerBool){
     fullBoard.insertAdjacentHTML('beforeend', `
         <svg viewBox="0 0 100 100" style="position:absolute; pointer-events:none; width:100%; height:100%; display:block;">
             <style>
                 .win-line{
-                    filter: drop-shadow(0px 1px 5px red);
+                    filter: drop-shadow(0px 1px 10px red);
                     stroke-dasharray:${dashArrayLength};
                     stroke-dashoffset:${dashArrayLength};
                     animation: DrawLine 0.6s cubic-bezier(0.4, 0, 0.4, 1) forwards;
@@ -96,7 +103,7 @@ const updateBoardAfterWin = (tripleAsked, playerBool) => {
         <svg viewBox="0 0 100 100" style="position:absolute; pointer-events:none; width:100%; height:100%; display:block;">
             <style>
                 .win-line{
-                    filter: drop-shadow(0px 1px 5px lightblue);
+                    filter: drop-shadow(0px 2px 10px lightblue);
                     stroke-dasharray:${dashArrayLength};
                     stroke-dashoffset:${dashArrayLength};
                     animation: DrawLine 0.6s cubic-bezier(0.4, 0, 0.4, 1) forwards;
@@ -114,26 +121,70 @@ const updateBoardAfterWin = (tripleAsked, playerBool) => {
     }
 };
 
+//Tie condition created if the whole board fills
 const tieCond = () => {
-    fullBoard.classList.add('grow-all');
+    for (const i of board)
+    {
+        if (i[0].innerHTML === '' || i[1].innerHTML === '' || i[2].innerHTML === '') return false;
+    }
+    return true;
+};
+
+const enlargeWinTriple = (askedTriple) => {
+    for (const i of askedTriple){
+        board[i[0]][i[1]].classList.add('winner');
+    }
 }
 
-let forPlayerO = true;
-let game;
+let forPlayerO = true; //Always 'O' plays the first
+let gameOver = false; //created game end scenario
+
+//created array for state check
+let stateArray = [
+    [null, null, null],
+    [null, null, null], 
+    [null, null, null]
+];
+
+//For each box, creating an event listener on clicking
 board.forEach(row => {
     row.forEach(col => {
         col.addEventListener('click', () => {
-            if (col.innerHTML === ``) forPlayerO = updateBoardWithPlayerShape(forPlayerO, col);
-            const [winTriple, isWin] = winCond();
-            if (isWin){
-                updateBoardAfterWin(winTriple, forPlayerO);
-                if (forPlayerO) console.log('Player X wins!');
-                else console.log('Player O wins');
+            const rowPosIndex = board.indexOf(row);
+            const colPosIndex = row.indexOf(col);
+
+            //Just if the box is empty update the board
+            if (!gameOver){
+                if (col.innerHTML === ``) forPlayerO = updateBoardWithPlayerShape(forPlayerO, col);
+
+                //pushed into the specific cols and rows
+                if (forPlayerO) stateArray[rowPosIndex][colPosIndex] = 'X';
+                else stateArray[rowPosIndex][colPosIndex] = 'O';
+
+                //storing return from the win condition function
+                const [winTriple, isWin] = winCond();
+                if (isWin){
+                    updateBoardAfterWin(winTriple, forPlayerO);
+                    if (forPlayerO) console.log('Player X wins!');
+                    else console.log('Player O wins');
+                    enlargeWinTriple(winTriple);
+                    gameOver = true;
+                }else{
+
+                    //storing return from the tie condition function
+                    const isTie = tieCond();
+                    if (isTie){
+                        fullBoard.classList.add('grow');
+                        gameOver = true;
+                    };
+                }
             }
-            else fullBoard.classList.add('grow-all');
+            
+            
         })
     })
 })
+console.log(stateArray);
 
 
 
